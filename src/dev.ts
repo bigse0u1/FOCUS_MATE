0ba8820778a6c31443c5afe2ad276bb6c019b7d2
Taekyeong Lee <lee.bigse0u1@gmail.com>
2025-12-13 14:40:31 +0900

    Update dev.ts

--- src/dev.ts
+++ src/dev.ts
@@ -1,10 +1,3 @@
-/**
- * 앱 진입점: Vision → Metrics → State → Main 초기화
- */
-import { Vision } from "./vision/index";
 import "./metrics/index";
 import "./state/index";
 import "./main";
-
-const vision = new Vision();
-vision.start();
